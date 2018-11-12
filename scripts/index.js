87d5b50aa8c2acff03bd742c024e2dc06d3e133d
Anton Yatskevich <anton_yatskevich@mail.ru>
2018-11-12 17:27:35 +0300

    add fields for date and source

--- scripts/index.js
+++ scripts/index.js
@@ -4,34 +4,54 @@ const articlesContainer = document.getElementById('news-articles-wrapper');
 
 function createArticle(articleData, parentNode) {
   const {
+    source,
     title,
     description,
     url,
     urlToImage,
+    publishedAt,
   } = articleData;
 
+  const publishDate = publishedAt.replace('T', ' ').slice(0, -4);
   if (title && description && url && urlToImage) {
     const article = document.createElement('article');
     article.innerHTML = `
       <a href="${url}"><img class="article-image" src=${urlToImage} alt="article-image"></a>
       <div class="article-info">
-          <a href="${url}"><h3>${title}</h3></a>
-          <p>${description}</p>
+          <a href="${url}"><h4>${title}</h4></a>
+          <div class="article-description">
+            <p>${description}</p>
+            <p class="article-description-footer"><a href='#'>${source.name}</a><span>${publishDate}</span></p>
+          </div>
+          
       </div>`;
     parentNode.appendChild(article);
   }
 }
 
-function createNewsList(data) {
+function createNewsList(data, headerText) {
   const wrapper = document.createDocumentFragment();
   const { articles } = data;
+  const newsListHeader = document.createElement('h3');
+  newsListHeader.textContent = headerText;
+  wrapper.appendChild(newsListHeader);
   articles.map(article => createArticle(article, wrapper));
   articlesContainer.appendChild(wrapper);
 }
 
-fetch(TOP_NEWS_URL)
-  .then(res => res.json())
-  .then(data => createNewsList(data))
-  .catch((err) => {
-    throw new Error(err);
-  });
+function getNews(url, headerText) {
+  fetch(url)
+    .then(res => res.json())
+    .then(data => createNewsList(data, headerText))
+    .catch((err) => {
+      throw new Error(err);
+    });
+}
+
+function getMostPopularNews() {
+  const newsListHeaderText = 'Most popular news:';
+
+  getNews(TOP_NEWS_URL, newsListHeaderText);
+}
+
+getMostPopularNews();
