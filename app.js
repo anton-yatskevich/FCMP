683f9741687ee95a9da729b937133852a5084af3
anton_yatskevich <anton_yatskevich@mail.ru>
2019-01-24 17:51:47 +0300

    feat: logger to file

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const router = require('./routes')
@@ -8,6 +9,8 @@ const port = 3000;
 
 const app = express();
 
+const accessLogStream = fs.createWriteStream(path.join(__dirname, './logs/server.log'), { flags: 'a' })
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -15,7 +18,7 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
-app.use(morgan(':method :url :status :date[web]'));
+app.use(morgan(':method :url :status :date[web]', { stream: accessLogStream }));
 app.use(router);
 app.use(errorHandler);
 
